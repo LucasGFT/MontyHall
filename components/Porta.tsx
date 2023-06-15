b95a9373de6a84d7bdf19111bc0a41a7e2820aa5
LucasGFT <lucasgft200303@gmail.com>
2023-06-15 15:11:50 -0300

    primeiro commit

---
+++ components/Porta.tsx
@@ -0,0 +1,13 @@
+import styles from '../src/styles/Porta.module.css'
+
+export default function Porta() {
+    return (
+        <div className={styles.area}>
+            <div className={styles.estrutura}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>3</div>
+                </div>
+            </div>
+        </div>
+    )
+}
