a16701b215d7550e2f4403bc785841046ae48444
LucasGFT <lucasgft200303@gmail.com>
2023-06-16 11:59:57 -0300

    Updates

--- prerender-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"cad0f7230f33a9481ea9cf0a363688b5\",\"previewModeSigningKey\":\"e74280e41ef045bb083064a7ce46b7b4cec0fc47ef8b287a9eed7ea8eb1e81a4\",\"previewModeEncryptionKey\":\"3f8c9b0e5016cb655c7b9d10ddccb1f10aba881be1499b34d87367688d2ac213\"},\"notFoundRoutes\":[]}"
